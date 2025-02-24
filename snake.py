99481b2fbc029fa0e1372c13c32d636563749c85
Meir Stroh <meirstroh6@gmail.com>
2025-02-23 20:55:41 -0500

    initial commit

---
+++ snake.py
@@ -0,0 +1,59 @@
+import curses
+import time
+import random
+
+def main(stdcr):
+    stdcr.clear()
+    curses.curs_set(0)
+    stdcr.nodelay(True)
+    stdcr.border()
+    
+    height, width = stdcr.getmaxyx()
+    direction = (0, 1)
+        
+    yrandom = random.randrange(0, height)
+    xrandom = random.randrange(0, width)
+    
+    y = height // 2
+    x = width // 2
+    snake = ["#"]
+    
+    snakePositions = [(y, x)]
+    
+    while True:
+        
+        stdcr.clear()
+        for i in range(len(snakePositions)):
+            stdcr.addstr(snakePositions[i][0], snakePositions[i][1], snake[0])
+            pos = str(len(snakePositions))
+            stdcr.addstr(0, 0, pos)
+        stdcr.addstr(yrandom, xrandom, "*")
+        stdcr.refresh()
+        
+        time.sleep(0.10)
+        
+        if y == yrandom and x == xrandom:
+            snake.insert(0, "#")
+            yrandom = random.randrange(0, height)
+            xrandom = random.randrange(0, width)
+        # snake.append("#")
+        
+        y += direction[0]
+        x += direction[1]
+        snakePositions.insert(0, (y, x))
+        # snakePositions.pop(-1)
+        
+        key = stdcr.getch()
+        if key == curses.KEY_DOWN and direction != (-1, 0):
+            direction = (1, 0)
+        elif key == curses.KEY_UP and direction != (1, 0):
+            direction = (-1 ,0 )
+        elif key == curses.KEY_RIGHT and direction != (0, -1):
+            direction = (0, 1)
+        elif key == curses.KEY_LEFT and direction != (0, 1):
+            direction = (0, -1)
+            
+        
+    
+    
+curses.wrapper(main)
